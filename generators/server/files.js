e2c42a6576cff3b13a33d4f3f7f5ef821209d30c
Anthony Viard <anth.viard@gmail.com>
2019-01-24 17:06:37 +0100

    Modify a file application.yml

--- generators/server/files.js
+++ generators/server/files.js
@@ -1,6 +1,7 @@
 const constants = require('generator-jhipster/generators/generator-constants');
 
 const SERVER_MAIN_RES_DIR = constants.SERVER_MAIN_RES_DIR;
+const JHI_CONFIG_FOLDER_PATH = `${SERVER_MAIN_RES_DIR}config/`;
 
 const serverFiles = {
     serverResource: [
@@ -16,8 +17,14 @@ function writeFiles() {
         writeSamFiles() {
             this.writeFilesToDisk(serverFiles, this, false);
         },
-        modifyFiles() {}
+        modifyFiles() {
+            addMyPropInApplicationYaml.call(this);
+        }
     };
+}
+
+function addMyPropInApplicationYaml() {
+    this.replaceContent(`${JHI_CONFIG_FOLDER_PATH}application.yml`, '# application:', '# application:\n    # myProp: true');
 }
 
 module.exports = {
